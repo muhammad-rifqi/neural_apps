d4a1c8ef7a835357c4b31dbe8e2f2cca73c12b6e
Muhammad Rifqi <muhammad45rifki@gmail.com>
2025-09-13 16:11:33 +0700

    update

---
+++ project_wise/project_apps.sql
@@ -0,0 +1,601 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Sep 13, 2025 at 11:05 AM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 7.4.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `project_apps`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `allocations`
+--
+
+CREATE TABLE `allocations` (
+  `id` int(11) NOT NULL,
+  `project_id` int(11) NOT NULL,
+  `sdlc_method_id` int(11) NOT NULL,
+  `duration_months` int(11) NOT NULL DEFAULT 0,
+  `total_development_cost` bigint(20) NOT NULL DEFAULT 0,
+  `additional_cost` bigint(20) NOT NULL DEFAULT 0,
+  `status` varchar(100) NOT NULL DEFAULT 'NULL',
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `last_updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `allocations`
+--
+
+INSERT INTO `allocations` (`id`, `project_id`, `sdlc_method_id`, `duration_months`, `total_development_cost`, `additional_cost`, `status`, `created_at`, `last_updated_at`) VALUES
+(2, 10, 1, 0, 2000000, 1000000, 'NULL', '2025-09-12 17:14:58', '2025-09-12 17:14:58');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `expertise_level`
+--
+
+CREATE TABLE `expertise_level` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `expertise_level`
+--
+
+INSERT INTO `expertise_level` (`id`, `name`) VALUES
+(1, 'Ahli'),
+(2, 'Menengah'),
+(3, 'Pemula');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `failed_jobs`
+--
+
+CREATE TABLE `failed_jobs` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `uuid` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `connection` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(1, '2014_10_12_000000_create_users_table', 1),
+(2, '2014_10_12_100000_create_password_resets_table', 1),
+(3, '2019_08_19_000000_create_failed_jobs_table', 1),
+(4, '2019_12_14_000001_create_personal_access_tokens_table', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_resets`
+--
+
+CREATE TABLE `password_resets` (
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `personal_access_tokens`
+--
+
+CREATE TABLE `personal_access_tokens` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `tokenable_type` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tokenable_id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `abilities` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `last_used_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `personal_access_tokens`
+--
+
+INSERT INTO `personal_access_tokens` (`id`, `tokenable_type`, `tokenable_id`, `name`, `token`, `abilities`, `last_used_at`, `created_at`, `updated_at`) VALUES
+(1, 'token_type', 12345, 'project_new', '', NULL, '2025-08-24 14:18:04', '2025-08-24 14:18:04', '2025-08-20 14:18:04');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `projects`
+--
+
+CREATE TABLE `projects` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `type_project` varchar(255) NOT NULL,
+  `scale` varchar(255) NOT NULL,
+  `start_date` date NOT NULL,
+  `end_date` date NOT NULL,
+  `create_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `last_updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `projects`
+--
+
+INSERT INTO `projects` (`id`, `name`, `type_project`, `scale`, `start_date`, `end_date`, `create_at`, `last_updated_at`) VALUES
+(10, 'Portal Berita', 'Hardware', 'Middle', '2025-09-01', '2025-09-30', '2025-09-12 17:14:58', '2025-09-12 17:14:58');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `project_categories`
+--
+
+CREATE TABLE `project_categories` (
+  `id` int(11) NOT NULL,
+  `project_id` int(11) NOT NULL,
+  `technology_type_id` int(11) NOT NULL DEFAULT 0,
+  `tools_name` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `project_categories`
+--
+
+INSERT INTO `project_categories` (`id`, `project_id`, `technology_type_id`, `tools_name`, `created_at`) VALUES
+(4, 10, 1, 'Erite', '2025-09-12 17:14:58');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `project_scale`
+--
+
+CREATE TABLE `project_scale` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `project_scale`
+--
+
+INSERT INTO `project_scale` (`id`, `name`) VALUES
+(1, 'Small'),
+(2, 'Middle'),
+(3, 'Big');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `project_type`
+--
+
+CREATE TABLE `project_type` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `project_type`
+--
+
+INSERT INTO `project_type` (`id`, `name`) VALUES
+(1, 'Sotware'),
+(2, 'Hardware'),
+(3, 'Security');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `risk`
+--
+
+CREATE TABLE `risk` (
+  `id` int(11) NOT NULL,
+  `project_id` int(11) NOT NULL DEFAULT 0,
+  `risk_type_id` int(11) NOT NULL DEFAULT 0,
+  `description` text DEFAULT NULL,
+  `impact_level` varchar(100) DEFAULT NULL,
+  `likelihood` varchar(100) DEFAULT NULL,
+  `mitigation_plan` text DEFAULT NULL,
+  `status` varchar(100) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `last_updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `risk`
+--
+
+INSERT INTO `risk` (`id`, `project_id`, `risk_type_id`, `description`, `impact_level`, `likelihood`, `mitigation_plan`, `status`, `created_at`, `last_updated_at`) VALUES
+(1, 10, 1, 'sdsdfsd', 'medium', 'unlikely', NULL, NULL, '2025-09-12 18:14:07', '2025-09-12 18:14:07');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `risk_categories`
+--
+
+CREATE TABLE `risk_categories` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `risk_categories`
+--
+
+INSERT INTO `risk_categories` (`id`, `name`) VALUES
+(1, 'Financial'),
+(2, 'Operational'),
+(3, 'Compliance');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `risk_types`
+--
+
+CREATE TABLE `risk_types` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `category_id` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `risk_types`
+--
+
+INSERT INTO `risk_types` (`id`, `name`, `description`, `category_id`, `created_at`) VALUES
+(1, 'Finance', 'Finance', 1, '2025-09-12 04:30:50'),
+(2, 'Operational', 'Operational', 2, '2025-09-12 04:31:19'),
+(3, 'Compliance', 'Compliance', 3, '2025-09-12 04:31:38');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sdlc_method`
+--
+
+CREATE TABLE `sdlc_method` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `sdlc_method`
+--
+
+INSERT INTO `sdlc_method` (`id`, `name`) VALUES
+(1, 'Method 1'),
+(2, 'Method 2');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `team_member`
+--
+
+CREATE TABLE `team_member` (
+  `id` int(11) NOT NULL,
+  `allocation_id` int(11) NOT NULL DEFAULT 0,
+  `role` varchar(100) DEFAULT NULL,
+  `quantity` int(11) NOT NULL DEFAULT 0,
+  `expertise_level_id` int(11) NOT NULL DEFAULT 0,
+  `avg_salary` int(11) NOT NULL DEFAULT 0,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `last_updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `team_member`
+--
+
+INSERT INTO `team_member` (`id`, `allocation_id`, `role`, `quantity`, `expertise_level_id`, `avg_salary`, `created_at`, `last_updated_at`) VALUES
+(1, 2, NULL, 12, 2, 1000000, '2025-09-12 17:52:02', '2025-09-12 17:52:02');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `technology_type`
+--
+
+CREATE TABLE `technology_type` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `technology_type`
+--
+
+INSERT INTO `technology_type` (`id`, `name`) VALUES
+(1, 'Type A'),
+(2, 'Type B');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `fullname` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `nick_name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `phone_number` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `company` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `fullname`, `nick_name`, `phone_number`, `company`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'Rifqi Muhammad', 'muhammad45rifki@gmail.com', NULL, '$2y$10$pUz1PVESITc5g826sxpBSOr1DH0JZpefY6w4VVTKsFfrsuOYyHPWm', 'Muhammad Rifqi', 'Rifqi', '+6212345678998', 'PT Jaya Abadi', NULL, '2025-06-30 22:26:46', '2025-06-30 22:26:46');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `allocations`
+--
+ALTER TABLE `allocations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `expertise_level`
+--
+ALTER TABLE `expertise_level`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`);
+
+--
+-- Indexes for table `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `password_resets`
+--
+ALTER TABLE `password_resets`
+  ADD KEY `password_resets_email_index` (`email`);
+
+--
+-- Indexes for table `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `personal_access_tokens_token_unique` (`token`),
+  ADD KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`);
+
+--
+-- Indexes for table `projects`
+--
+ALTER TABLE `projects`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `project_categories`
+--
+ALTER TABLE `project_categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `project_scale`
+--
+ALTER TABLE `project_scale`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `project_type`
+--
+ALTER TABLE `project_type`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `risk`
+--
+ALTER TABLE `risk`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `risk_categories`
+--
+ALTER TABLE `risk_categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `risk_types`
+--
+ALTER TABLE `risk_types`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `sdlc_method`
+--
+ALTER TABLE `sdlc_method`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `team_member`
+--
+ALTER TABLE `team_member`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `technology_type`
+--
+ALTER TABLE `technology_type`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_email_unique` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `allocations`
+--
+ALTER TABLE `allocations`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `expertise_level`
+--
+ALTER TABLE `expertise_level`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `projects`
+--
+ALTER TABLE `projects`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `project_categories`
+--
+ALTER TABLE `project_categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `project_scale`
+--
+ALTER TABLE `project_scale`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `project_type`
+--
+ALTER TABLE `project_type`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `risk`
+--
+ALTER TABLE `risk`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `risk_categories`
+--
+ALTER TABLE `risk_categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `risk_types`
+--
+ALTER TABLE `risk_types`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `sdlc_method`
+--
+ALTER TABLE `sdlc_method`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `team_member`
+--
+ALTER TABLE `team_member`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `technology_type`
+--
+ALTER TABLE `technology_type`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
