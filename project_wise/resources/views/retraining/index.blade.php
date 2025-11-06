cc718bc2df1747aeec559ca1afa235b91f2edd35
Muhammad Rifqi <muhammad45rifki@gmail.com>
2025-11-06 11:53:10 +0700

    udpate

--- project_wise/resources/views/retraining/index.blade.php
+++ project_wise/resources/views/retraining/index.blade.php
@@ -11,7 +11,7 @@
     .metrics p { margin: 6px 0; }
   </style>
 
-    <div class="container" style="height: 1200px; overflow-y:scroll;scrollbar-width: none;">
+    <div class="container" style="height: 1200px; overflow-y:scroll;scrollbar-width: none;" id="warna_text">
         <h1 align="center" style="margin-top: 80px;">Great to see you, {{Auth::user()->name}}! Let’s boost performance with retraining.</h1>
         <p class="subtitle" align="center">
         Configure and initiate the retraining process to update and improve the accuracy of your predictive machine learning models.
